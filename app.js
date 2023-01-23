a4c67152f3768ce1b44b849d1f2f0ed7c8585d46
OskarKuchta <oskarkuchta5@gmial.com>
2023-01-23 10:55:51 +0100

    First value in array

--- app.js
+++ app.js
@@ -46,3 +46,11 @@ cubes = (a) => {
 console.log(cubes(3));
 console.log(cubes(5));
 console.log(cubes(10));
+
+//First value in array 
+getFirstValue = (a) => {
+    return a[0];
+}
+console.log(getFirstValue([1,2,3]));
+console.log(getFirstValue([80,5,100]));
+console.log(getFirstValue([-500, 0, 50]));
