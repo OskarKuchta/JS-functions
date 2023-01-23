c4a29d16cf25681b3c3785392b8f33b0278928ef
OskarKuchta <oskarkuchta5@gmial.com>
2023-01-23 10:42:05 +0100

    Add one to number

--- app.js
+++ app.js
@@ -13,3 +13,12 @@ convert = (a) => {
 console.log(convert(5));
 console.log(convert(3));
 console.log(convert(2));
+
+// Add one to something number // 
+addition = (a) => {
+    return a += 1;
+}
+
+console.log(addition(0));
+console.log(addition(9));
+console.log(addition(-3));
