3f5d3d10119016bae6a754f27cd54eb788a58354
OskarKuchta <oskarkuchta5@gmial.com>
2023-01-23 10:35:23 +0100

    Convert minutes to seconds

--- app.js
+++ app.js
@@ -2,8 +2,15 @@
 addition = (a,b) => {
     return a + b;
 }
-
 console.log(addition(3, 4)); 
 console.log(addition(-3, -6));
 console.log(addition(7, 3));
+*/
+/* Convert minutes to seconds
+convert = (a) => {
+    return a *= 60;
+}
+console.log(convert(5));
+console.log(convert(3));
+console.log(convert(2));
 */
