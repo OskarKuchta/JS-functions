1b8ded9ab01c1c2ec3140a7234d46aab88467f1c
OskarKuchta <oskarkuchta5@gmial.com>
2023-01-23 10:38:20 +0100

    Remove comment

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
-/* Summary two numbers
+// Summary two numbers //
 addition = (a,b) => {
     return a + b;
 }
 console.log(addition(3, 4)); 
 console.log(addition(-3, -6));
 console.log(addition(7, 3));
-*/
-/* Convert minutes to seconds
+
+//Convert minutes to seconds //
 convert = (a) => {
     return a *= 60;
 }
 console.log(convert(5));
 console.log(convert(3));
 console.log(convert(2));
-*/
