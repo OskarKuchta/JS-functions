81544e12e9584115295fadb6500efb35ca71844b
OskarKuchta <oskarkuchta5@gmial.com>
2023-01-24 10:29:43 +0100

    Compare summary two numbers to another

--- app.js
+++ app.js
@@ -145,5 +145,11 @@ nameString = (name) => {
 console.log(nameString("Mubashir"));
 console.log(nameString("Matt"));
 console.log(nameString("javascript"));
-   
-   
+
+// Compare summary two numbers to another
+lessThan100 = (a, b) => {
+ return (a + b) < 100 ? true : false;
+}
+console.log(lessThan100(22, 15));
+console.log(lessThan100(83, 34));
+console.log(lessThan100(3, 77));
