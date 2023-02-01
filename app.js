a582d60fdc6b7c042e2559f7003219291cfe36fe
OskarKuchta <oskarkuchta5@gmial.com>
2023-02-01 20:27:28 +0100

    Radians to Degrees

--- app.js
+++ app.js
@@ -442,3 +442,12 @@ eq = (a) => {
 console.log(eq("1+2"));
 console.log(eq("6/(9-7)"));
 console.log(eq("3+2-4"));
+
+// Radians to Degrees
+radiansToDegrees = (a) => {
+  return a *= 57.29577951308232;
+}
+console.log(radiansToDegrees(1));
+console.log(radiansToDegrees(20));
+console.log(radiansToDegrees(50));
+
