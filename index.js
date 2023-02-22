2bc42d7c5e7aff8fdc1703f49d4fe6326987d4ee
OskarKuchta <oskarkuchta5@gmial.com>
2023-02-22 11:47:04 +0100

    Scrabble Hand

--- index.js
+++ index.js
@@ -1883,3 +1883,29 @@ add = (x) => (y) => x + y;
 console.log(add(10)(20));
 console.log(add(0)(20));
 console.log(add(-30)(80));
+
+// Scrabble Hand
+Scrable = (a) => {
+  let number = 0;
+  return a.reduce((first, second) => {
+    return first + second.score
+  }, number)
+}
+console.log(Scrable([
+  { tile: "N", score: 1 },
+  { tile: "K", score: 5 },
+  { tile: "Z", score: 10 },
+  { tile: "X", score: 8 },
+  { tile: "D", score: 2 },
+  { tile: "A", score: 1 },
+  { tile: "E", score: 1 }]));
+console.log(Scrable([
+  { tile: "B", score: 2 },
+  { tile: "V", score: 4 },
+  { tile: "F", score: 4 },
+  { tile: "U", score: 1 },
+  { tile: "D", score: 2 },
+  { tile: "O", score: 1 },
+  { tile: "U", score: 1 }
+]));
+
