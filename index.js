23a39181f5856c563bf8bcdf3e26fc8f0274a188
OskarKuchta <oskarkuchta5@gmial.com>
2023-02-25 13:44:33 +0100

    Phone Number Formatting

--- index.js
+++ index.js
@@ -2001,3 +2001,15 @@ console.log(isSymmetrical(7227));
 console.log(isSymmetrical(12567));
 console.log(isSymmetrical(44444444));
 console.log(isSymmetrical(1112111));
+
+// Phone Number Formatting
+formatPhoneNumber = (a) => {
+  let change = a.join("");
+  let first = change.slice(0, 3);
+  let second = change.slice(2, 5);
+  let third = change.slice(5, 9)
+  return "(" + first + ") " + second + "-" + third;
+}
+console.log(formatPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]));
+console.log(formatPhoneNumber([5, 1, 9, 5, 5, 5, 4, 4, 6, 8]));
+console.log(formatPhoneNumber([3, 4, 5, 5, 0, 1, 2, 5, 2, 7]));
