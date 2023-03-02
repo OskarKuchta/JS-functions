f078cdb938c5d1c963a63171d3d7ad1238523d2a
Oskar Kuchta <oskarkuchta5@gmail.com>
2023-03-02 18:34:03 +0100

    Finding Nemo

--- src/index2.js
+++ src/index2.js
@@ -1,1 +1,13 @@
-console.log(123443);
+// Finding Nemo
+const findNemo = (a) => {
+    if(a.match(/nemo/gi)) {
+        return `I found Nemo at ${a.split(' ').indexOf("Nemo") + 1}!`;
+    }
+    else {
+        return "I can't find Nemo :(";
+    }
+}
+console.log(findNemo("I am finding Nemo !"));
+console.log(findNemo("Nemo is me"));
+console.log(findNemo("I Nemo am"));
+console.log(findNemo("Don't have nem o"));
