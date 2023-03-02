b95bbd1da8129be9279f9b64efb2daf46b9affd6
Oskar Kuchta <oskarkuchta5@gmail.com>
2023-03-02 18:47:55 +0100

    Move Capital Letters to the Front

--- src/index2.js
+++ src/index2.js
@@ -21,3 +21,11 @@ const marathonDistance = (a) => {
 console.log(marathonDistance([1, 2, 3, 4]));
 console.log(marathonDistance([1, 9, 5, 8, 2]));
 console.log(marathonDistance([-6, 15, 4]));
+
+// Move Capital Letters to the Front
+capToFront = (a) => {
+    return (a.match(/[A-Z]/g) + a.match(/[a-z]/g)).replaceAll(",", "");
+}
+console.log(capToFront("hApPy"));
+console.log(capToFront("moveMENT"));
+console.log(capToFront("shOrtCAKE"));
