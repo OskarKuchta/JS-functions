4eb0982358640911a3cbdbc6128242d427e7202a
Oskar Kuchta <oskarkuchta5@gmail.com>
2023-03-02 18:41:52 +0100

    25-Mile Marathon

--- src/index2.js
+++ src/index2.js
@@ -11,3 +11,13 @@ console.log(findNemo("I am finding Nemo !"));
 console.log(findNemo("Nemo is me"));
 console.log(findNemo("I Nemo am"));
 console.log(findNemo("Don't have nem o"));
+
+// 25-Mile Marathon
+const marathonDistance = (a) => {
+    let absolute = a.map((element) => Math.abs(element));
+    let count = absolute.reduce((item, sum) => item + sum);
+    return count >= 25 ? true : false;
+}
+console.log(marathonDistance([1, 2, 3, 4]));
+console.log(marathonDistance([1, 9, 5, 8, 2]));
+console.log(marathonDistance([-6, 15, 4]));
