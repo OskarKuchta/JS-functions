76fd42dfae1a690e7894b31b3e803878a226c322
Oskar Kuchta <oskarkuchta5@gmail.com>
2023-03-10 14:19:28 +0100

    Set and Map

--- learn/index.js
+++ learn/index.js
@@ -19,3 +19,19 @@ console.log(myAge);
 console.log(person);
 console.log(car);
 
+const setNumbers = new Set([1,3,5,7]); // unique values
+setNumbers.add(9);
+setNumbers.add(11);
+setNumbers.add(myAge);
+setNumbers.delete(1);
+console.log(setNumbers);
+
+const mapNumbers = new Map([
+   ["first", 1],
+   ["second", 3],
+   ["third", 5]
+]);
+mapNumbers.set("fourth",9);
+console.log(mapNumbers);
+console.log(mapNumbers.get("fourth")); // function work only on keys, not on values
+console.log(mapNumbers.has("first")); 
