6c9ec41435860083344d8b3a2f70b647197e18af
Oskar Kuchta <oskarkuchta5@gmail.com>
2023-03-10 13:58:57 +0100

    Import export

--- learn/index.js
+++ learn/index.js
@@ -1,3 +1,5 @@
+import myAge from "./index2.js"
+import { person, car } from "./index2.js";
 let promise = new Promise((resolve,reject) => {
     const name = "Oskar";
     if(2 >5) {
@@ -11,4 +13,9 @@ promise.then((message) => {
     console.log(message);
 }).catch((message) => {
     console.log(message + " because 2 is smaller than 5");
-})
+});
+// let car; Car was declared in second file, name variable was reserved 
+console.log(myAge);
+console.log(person);
+console.log(car);
+
---
+++ learn/index2.js
@@ -0,0 +1,12 @@
+let age = 24;
+export let person = {
+    name: "Oskar",
+    age: 24,
+    height: 176
+}
+export let car = {
+    type: "sedan",
+    model: "audi",
+    year: 2023
+}
+export default age;
